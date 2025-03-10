fc9c5fbe4b00b47e7ba00bae59735b85380b7255
Nick-2003 <tsunlinicholastam@gmail.com>
2025-03-10 12:45:09 -0700

    Create code folder; haven't implemented into .qmd yet

---
+++ code/03-model.R
@@ -0,0 +1,44 @@
+# Importing required packages for analysis. Suppress warnings and startup messages the first time libraries are loaded
+library(tidyverse) # Data wrangling and visualization
+library(tidymodels) # Machine learning tools
+library(glmnet) # Fit generalized linear models by penalty
+library(patchwork) # Combine plots
+library(ROSE) # Random Over-Sampling Examples for dataset balancing
+library(vcd) # For Cramér’s V
+
+# READ diabetes_train, diabetes_test
+
+# Selecting only the features we determined from 3.2. EDA - Feature Selection and Visualization
+diabetes_train_filtered <- diabetes_train %>%
+  select(Diabetes_binary, GenHlth, HighBP, Age, HighChol, DiffWalk)
+
+# Pipeline for logistic regression 
+lr_mod <- logistic_reg(penalty = tune(), mixture = 1) %>% 
+    set_engine("glmnet") %>%
+    set_mode("classification")
+
+folds <- vfold_cv(diabetes_train_filtered, v=5)
+
+lr_recipe <- recipe(Diabetes_binary ~ ., data = diabetes_train_filtered) %>%
+  step_dummy(all_nominal_predictors(), -all_ordered()) %>% 
+  step_normalize(all_predictors())
+
+lr_workflow <- workflow() %>%
+  add_recipe(lr_recipe)
+
+# Tuning with cross-validation set for penalty
+lambda_grid <- grid_max_entropy(penalty(), size = 10)
+
+lasso_grid <- tune_grid(lr_workflow %>% add_model(lr_mod),
+                        resamples = folds,
+                        grid = lambda_grid,
+                        metrics = metric_set(recall))
+
+# Choosing the metric with the highest recall
+highest_auc <- lasso_grid %>% select_best(metric = "recall")
+
+lasso_tuned_wflow <- finalize_workflow(lr_workflow %>% 
+                                         add_model(lr_mod),highest_auc) %>%
+  fit(data = diabetes_train_filtered)
+
+# WRITE lasso_tuned_wflow
---
+++ code/04-analysis.R
@@ -0,0 +1,67 @@
+# Importing required packages for analysis. Suppress warnings and startup messages the first time libraries are loaded
+library(tidyverse) # Data wrangling and visualization
+library(tidymodels) # Machine learning tools
+library(glmnet) # Fit generalized linear models by penalty
+library(patchwork) # Combine plots
+library(ROSE) # Random Over-Sampling Examples for dataset balancing
+library(vcd) # For Cramér’s V
+
+# READ diabetes_train, diabetes_test, lasso_tuned_wflow
+
+# Applying to the test set
+lasso_preds <- lasso_tuned_wflow %>% predict(diabetes_test)
+lasso_probs <- lasso_tuned_wflow %>% predict(diabetes_test, type="prob")
+lasso_modelOutputs <- cbind(diabetes_test, lasso_preds, lasso_probs)
+
+classificationMetrics <- metric_set(sens, spec, ppv, npv, accuracy, recall, f_meas)
+roc_auc_value <- roc_auc(lasso_modelOutputs, truth = Diabetes_binary, .pred_1, event_level = "second")
+
+lasso_metrics <- rbind(classificationMetrics(lasso_modelOutputs, truth = Diabetes_binary, estimate = .pred_class, event_level = "second"),
+                       roc_auc_value)
+
+# WRITE lasso_metrics
+
+options(repr.plot.width = 8, repr.plot.height = 8)
+
+# Creating the ROC curve
+roc_plot <- autoplot(roc_curve(lasso_modelOutputs, Diabetes_binary, .pred_1, event_level = "second")) +
+  ggtitle("Figure 2. ROC Curve for Lasso Model") +
+  labs(subtitle = "Performance of the Lasso Model in Predicting Diabetes State.",
+       x = "False Positive Rate (1 - Specificity)",
+       y = "True Positive Rate (Sensitivity)") +
+  annotate("text", x = 0.7, y = 0.2, label = paste("AUC =", round(roc_auc_value$.estimate, 3)), size = 5, color = "blue") +
+  theme(
+    plot.title = element_text(size = 16, face = "bold"),
+    plot.subtitle = element_text(size = 12),
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 10)
+  )
+
+# WRITE roc_plot
+
+options(repr.plot.width = 8, repr.plot.height = 8)
+
+# Creating the confusion matrix
+cm <- conf_mat(lasso_modelOutputs, truth = Diabetes_binary, estimate = .pred_class, event_level = "second")
+cm_df <- as.data.frame(cm$table)
+
+cm_plot <- ggplot(cm_df, aes(x = Prediction, y = Truth, fill = Freq)) +
+  geom_tile() +
+  geom_text(aes(label = Freq), color = "black", size = 5) +
+  scale_fill_gradient(low = "white", high = "#66B2FF") +
+  ggtitle("Figure 3. Confusion Matrix for Lasso Model") +
+  labs(subtitle = "Performance of the Lasso Model in Predicting diabetic (1) or non-diabetic (0).", 
+       x = "Predicted Class", 
+       y = "True Class",
+       fill = "Count") + 
+  theme(
+    plot.title = element_text(size = 16, face = "bold"),
+    plot.subtitle = element_text(size = 12),
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 10),
+    plot.background = element_blank(),
+    panel.grid = element_blank()
+  ) +
+  guides(fill = "none")
+
+# WRITE cm_plot
