17fdfefe54da23aa4d8b41066e5854d0190869d3
Nick-2003 <tsunlinicholastam@gmail.com>
2025-03-29 18:58:57 -0700

    cm_plot needs fixing
    NOTE THAT cm_plot HAS NOT BEEN IMPLEMENTED YET

--- src/04-analysis.R
+++ src/04-analysis.R
@@ -9,28 +9,28 @@ Options:
 --output_path_lasso=<output_path_lasso>     Path to save the lasso_metrics
 --output_path_roc=<output_path_roc>         Path to save the ROC curve
 --output_path_cm=<output_path_cm>           Path to save the confusion matrix
---output_path_cm_df=<output_path_cm_df>     Path to save values from the confusion matrix
+--output_path_cm_df=<output_path_cm_df>   Path to save values from the confusion matrix
 
 " -> doc
+
+opt <- docopt::docopt(doc)
 
 library(tidyverse)
 library(tidymodels)
 library(glmnet)
 library(docopt)
-source("work/R/roc_plot.R")
-set.seed(6)
-
-opt <- docopt::docopt(doc)
-
 source(opt$r_path_roc_plot)
 source(opt$r_path_cm_plot)
+set.seed(6)
+
+
 
 # READ diabetes_test, lasso_tuned_wflow
 diabetes_test <- readr::read_rds(opt$file_path_test)
 lasso_tuned_wflow <- readr::read_rds(opt$file_path_wflow)
 
 # Applying to the test set
-lasso_model_outputs <- diabetes_test %>%
+lasso_modelOutputs <- diabetes_test %>%
   cbind(lasso_tuned_wflow %>% predict(diabetes_test), 
         lasso_tuned_wflow %>% predict(diabetes_test, type = "prob")) %>%
   mutate(Diabetes_binary = as.factor(Diabetes_binary))
@@ -38,13 +38,13 @@ lasso_model_outputs <- diabetes_test %>%
 # classification metrics
 lasso_metrics <- bind_rows(
   metric_set(sens, ppv, npv, accuracy, recall, f_meas)(
-    lasso_model_outputs,
+    lasso_modelOutputs,
     truth = Diabetes_binary,
     estimate = .pred_class,
     event_level = "second"
   ),
   roc_auc(
-    lasso_model_outputs,
+    lasso_modelOutputs,
     truth = Diabetes_binary,
     .pred_1,
     event_level = "second"
@@ -61,42 +61,44 @@ readr::write_csv(lasso_metrics, opt$output_path_lasso)
 
 # Creating the ROC curve # CONVERT TO FUNCTION roc_plot
 roc_plot(
-  model_outputs=lasso_model_outputs, 
+  model_outputs=lasso_modelOutputs, 
   true_class="Diabetes_binary", 
   predicted_probs=".pred_1", 
   roc_auc_value=lasso_metrics$.estimate[lasso_metrics$.metric == "roc_auc"], 
   output_path=opt$output_path_roc
 )
 
+
 # Creating the confusion matrix # CONVERT TO FUNCTION cm_plot
 options(repr.plot.width = 8, repr.plot.height = 8)
 
-cm <- yardstick::conf_mat(lasso_model_outputs, truth = Diabetes_binary, estimate = .pred_class, event_level = "second")
-# cm_df <- as.data.frame(cm$table)
+cm <- yardstick::conf_mat(lasso_modelOutputs, truth = Diabetes_binary, estimate = .pred_class, event_level = "second")
+cm_df <- as.data.frame(cm$table)
 
-# cm_plot <- ggplot2::ggplot(cm_df, ggplot2::aes(x = Prediction, y = Truth, fill = Freq)) +
-#   ggplot2::geom_tile() +
-#   ggplot2::geom_text(ggplot2::aes(label = Freq), color = "black", size = 5) +
-#   ggplot2::scale_fill_gradient(low = "white", high = "#66B2FF") +
-#   ggplot2::labs(
-#     x = "Predicted Class",
-#     y = "True Class",
-#     fill = "Count"
-#   ) +
-#   ggplot2::theme(
-#     plot.title = ggplot2::element_text(size = 16, face = "bold"),
-#     plot.subtitle = ggplot2::element_text(size = 12),
-#     axis.title = ggplot2::element_text(size = 12),
-#     axis.text = ggplot2::element_text(size = 10),
-#     plot.background = ggplot2::element_blank(),
-#     panel.grid = ggplot2::element_blank()
-#   ) +
-#   ggplot2::guides(fill = "none")
+cm_plot <- ggplot2::ggplot(cm_df, ggplot2::aes(x = Prediction, y = Truth, fill = Freq)) +
+  ggplot2::geom_tile() +
+  ggplot2::geom_text(ggplot2::aes(label = Freq), color = "black", size = 5) +
+  ggplot2::scale_fill_gradient(low = "white", high = "#66B2FF") +
+  ggplot2::labs(
+    x = "Predicted Class",
+    y = "True Class",
+    fill = "Count"
+  ) +
+  ggplot2::theme(
+    plot.title = ggplot2::element_text(size = 16, face = "bold"),
+    plot.subtitle = ggplot2::element_text(size = 12),
+    axis.title = ggplot2::element_text(size = 12),
+    axis.text = ggplot2::element_text(size = 10),
+    plot.background = ggplot2::element_blank(),
+    panel.grid = ggplot2::element_blank()
+  ) +
+  ggplot2::guides(fill = "none")
 
-cm_viz <- cm_plot(cm)
+# confusion_matrix_plot <- cm_plot(cm, "lasso_modelOutputs", "Diabetes_binary", ".pred_class", "Confusion Matrix", "#66B2FF")
 
 # WRITE cm_plot
 ggplot2::ggsave(opt$output_path_cm, cm_plot, width = 8, height = 8, dpi = 300, limitsize = FALSE)
+# ggplot2::ggsave(opt$output_path_cm, confusion_matrix_plot, width = 8, height = 8, dpi = 300, limitsize = FALSE)
 
 # WRITE cm_df
 readr::write_csv(cm_df, opt$output_path_cm_df)
