d0f905e2cf38bc63337ef774c7f0280800c2e436
Nick-2003 <tsunlinicholastam@gmail.com>
2025-04-11 15:34:24 -0700

    Try except added

--- src/dataset_download.py
+++ src/dataset_download.py
@@ -1,18 +1,21 @@
 import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
-# Fetch dataset
-cdc_diabetes_health_indicators = fetch_ucirepo(id=891)
+try: 
+    # Fetch dataset
+    cdc_diabetes_health_indicators = fetch_ucirepo(id=891)
+except: 
+    print('Cannot fetch dataset from UC Irvine Machine Learning Repository')
+else: 
+    # Data (as pandas DataFrames)
+    X = cdc_diabetes_health_indicators.data.features
+    y = cdc_diabetes_health_indicators.data.targets
 
-# Data (as pandas DataFrames)
-X = cdc_diabetes_health_indicators.data.features
-y = cdc_diabetes_health_indicators.data.targets
+    # Metadata (remove comment if metadata is required to be listed)
+    # print(cdc_diabetes_health_indicators.metadata)
 
-# Metadata (remove comment if metadata is required to be listed)
-# print(cdc_diabetes_health_indicators.metadata)
+    # Variable information (remove comment if metadata is required to be listed)
+    # print(cdc_diabetes_health_indicators.variables)
 
-# Variable information (remove comment if metadata is required to be listed)
-# print(cdc_diabetes_health_indicators.variables)
-
-df = pd.concat([X, y], axis=1)
-df.to_csv('~/work/data/raw/cdc_diabetes_health_indicators.csv', index=False) # Running from base
+    df = pd.concat([X, y], axis=1)
+    df.to_csv('~/work/data/raw/cdc_diabetes_health_indicators.csv', index=False) # Running from base
